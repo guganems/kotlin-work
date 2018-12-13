9a4caeb90e9994b36b3fe6465985728de608b634
gnems <g_nemsveridze@cu.edu.ge>
2018-12-13 14:49:48 +0400

    Loops

---
+++ untitled/src/loopDemo.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    // for loop
+//    for (i in 0..10){
+//        println(i)
+//    }
+//
+//    // while loop
+//    var i = 0
+//    while (i <= 10){
+//        println(i)
+//        i += 2
+//    }
+
+    // do..while
+    var j = 0
+    do{
+        println(j)
+        j += 5
+    }while (j <= 100)
+}
