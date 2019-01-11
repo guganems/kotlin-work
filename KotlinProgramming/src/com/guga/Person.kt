af37a8d82048be1fbc78a34092565dc9844ab35f
gnems <g_nemsveridze@cu.edu.ge>
2019-01-11 20:41:02 +0400

    Constructors

---
+++ KotlinProgramming/src/com/guga/Person.kt
@@ -0,0 +1,7 @@
+package com.guga
+
+class Person(var name: String) {
+    fun display(){
+        print("The name of the person is ${name}")
+    }
+}
--- KotlinProgramming/src/myfirstfile.kt
+++ KotlinProgramming/src/myfirstfile.kt
@@ -1,11 +1,6 @@
+import com.guga.Person
+
 fun main(args : Array<String>){
-    var personObj = Person()
-    personObj.name = "Steve"
-
-    print("The name of the person is ${personObj.name}")
-}
-
-class Person{
-
-    var name: String = ""
+    var personObj = Person("Steve")
+    personObj.display()
 }
